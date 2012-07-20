3bcaef4da1201abf3955ad54539b59c1f1c49e18
davids5 <david_s5@nscdg.com>
2012-07-20 11:28:49 -1000

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,9 +16,17 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
   def current_user?(user)
     @current_user == user
   end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in." unless signed_in?
+      end
+    end
 
   def signed_in?
     !current_user.nil?
